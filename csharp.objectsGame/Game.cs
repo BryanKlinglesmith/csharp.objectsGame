051ce18a7d280497396140874d220f56a6dadeaf
BryanKlinglesmith <b.klinglesmith@yahoo.com>
2018-04-11 17:56:39 -0400

    first commit

---
+++ csharp.objectsGame/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp.objectsGame
+{
+    class Game
+    {
+        public void Main()
+        {
+            Map map = new Map();
+
+            map.Width = 8;
+            map.Height = 5;
+
+            int area = map.Width * map.Height;
+
+            //Tower tower = new Tower(); //the first Tower is the class that is called. Second tower is a variable with a class of "Tower". after the = is what the variable becomes.
+        }
+    }
+}
